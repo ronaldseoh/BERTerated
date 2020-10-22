eb7c9e1555a93c74c4dce65378241beb7272ade2
Ronald Seoh <ronaldseoh@icloud.com>
2020-10-22 01:12:52 -0400

    copyright information

--- emnist_fedavg_main.py
+++ emnist_fedavg_main.py
@@ -1,4 +1,5 @@
 # Copyright 2020, The TensorFlow Federated Authors.
+# Copyright 2020, Ronald Seoh
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
