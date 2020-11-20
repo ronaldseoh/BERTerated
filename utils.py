d81bfc04bdc7499ce8fe0e1073f11815e5f327e9
Ronald Seoh <ronaldseoh@icloud.com>
2020-11-20 01:16:53 -0500

    utils.py: use mlm weights from the pretrained model

--- utils.py
+++ utils.py
@@ -121,5 +121,7 @@ def convert_huggingface_mlm_to_keras(huggingface_model, max_seq_length, batch_si
     
     mlm_output = mlm_layer(main_layer_output[0])
 
+    mlm_layer.set_weights(huggingface_model.layers[1].get_weights())
+
     return tf.keras.Model(input_ids, mlm_output)
 
