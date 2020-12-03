550f19f46d2109c28ed551fd8caf58fcc06dc0f9
Ronald Seoh <ronaldseoh@icloud.com>
2020-12-03 14:06:43 -0500

    misc/plot_generation_snippet.py: color scheme update

--- misc/plot_generation_snippet.py
+++ misc/plot_generation_snippet.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 
-
+plt.figure()
 plt.xlabel("Rounds")
 plt.ylabel("Loss")
 
-plt.plot(tb_events_df['train_loss'], label='Training loss')
+plt.plot(tb_events_df['train_loss'], label='Training loss', c='orangered')
 
-plt.plot(tb_events_df['validation_metric'], label='Validation loss')
+plt.plot(tb_events_df['validation_metric'], label='Validation loss', c='steelblue')
 
 for i in range(50):
     client_losses = training_loop_client_loss.iloc[i]
     client_losses_nonzero = client_losses[client_losses > 0]
-    plt.scatter([i+1] * len(client_losses_nonzero), client_losses_nonzero, c='gray')
+    plt.scatter([i+1] * len(client_losses_nonzero), client_losses_nonzero, c='silver')
 
-plt.show()
+
+plt.legend()
+
+plt.savefig('Figures/plot.png')
+plt.close()
